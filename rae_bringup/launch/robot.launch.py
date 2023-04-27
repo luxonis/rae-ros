9b846cd37cbbc4ecee8453271b2b0b97d8662133
Serafadam <serafadam@gmail.com>
2023-04-27 15:11:19 +0000

    add bringup dir

---
+++ rae_bringup/launch/robot.launch.py
@@ -0,0 +1,19 @@
+import os
+from ament_index_python.packages import get_package_share_path
+import launch
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    camera_prefix = get_package_share_path('rae_camera')
+    hw_prefix = get_package_share_path('rae_hw')
+    return launch.LaunchDescription([
+        IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(camera_prefix, 'launch', 'camera.launch.py'))),
+            IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(hw_prefix, 'launch', 'rae_control.launch.py'))),
+    ])
