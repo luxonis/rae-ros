5fa23b30138af892f9c03bcc129a5be32c5ba462
Danilo Pejovic <pejovicdanilo@outlook.com>
2023-06-26 11:37:22 +0200

    Testing, removing debugging tools

--- rae_hw/test/test_encoders.cpp
+++ rae_hw/test/test_encoders.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[]){
     int encRatioL = 187;
     int encRatioR = 187;
-
+    rae_hw::PID pid;
     if(argc == 2 && std::strcmp( argv[1], "-h" ) == 0 ){
         std::cout << "Help:\n";
         std::cout << "Positional arguments only for now.\n";
@@ -28,18 +28,16 @@ int main(int argc, char *argv[]){
     }
     std::cout << "Starting test procedure.\n";
     std::cout << "Enc ratios - L: " << encRatioL << " R: " << encRatioR << " counts/rev." << std::endl;
-    auto motorR = std::make_unique<rae_hw::RaeMotor>("right_wheel_name",
-                                        "gpiochip0", "/sys/class/pwm/pwmchip0", 2, 45, 46, 47, encRatioR, 32, false, 0);
     auto motorL = std::make_unique<rae_hw::RaeMotor>("left_wheel_name",
-                                        "gpiochip0", "/sys/class/pwm/pwmchip0", 1, 41, 42, 43, encRatioL, 32, false, 0);
+                                        "gpiochip0","/sys/class/pwm/pwmchip0", 1, 41, 42, 43, encRatioL, 32, true,false, pid);
+    auto motorR = std::make_unique<rae_hw::RaeMotor>("right_wheel_name",
+                                        "gpiochip0","/sys/class/pwm/pwmchip0", 2, 45, 46, 47, encRatioR, 32, false,false, pid);
     motorL->run();
     motorR->run();
     float prevPosL = 0.0;
     float prevPosR = 0.0;
-    float duration = 6.0;
-    auto startTime = std::chrono::high_resolution_clock::now();
-    bool timePassed = false;
-    while(!timePassed){
+    bool duration=true;
+    while(duration){
         auto leftPos = motorL->getPos();
         auto rightPos = motorR->getPos();
         if (leftPos!=prevPosL || rightPos != prevPosR){
@@ -48,13 +46,8 @@ int main(int argc, char *argv[]){
         }
         prevPosL = leftPos;
         prevPosR = rightPos;
-        auto currTime = std::chrono::high_resolution_clock::now();
-        float timeDiff = std::chrono::duration<float>(currTime - startTime).count();
-        if (timeDiff > duration){
-            timePassed = true;
-        }
+
     }
-    std::cout << "Left speed: " << (prevPosL/duration) << " rad.\n";
-    std::cout <<  "Right speed: " << (prevPosR/duration) << " rad." << std::endl;
+
     return 0;
 }
