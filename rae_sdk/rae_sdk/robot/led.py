ee9b59937973f34eae27c8e9a1e1c2847c79da40
Danilo Pejovic <115164734+danilo-pejovic@users.noreply.github.com>
2024-01-16 21:04:51 +0100

    Hotfix for LEDs pulsing by default

--- rae_sdk/rae_sdk/robot/led.py
+++ rae_sdk/rae_sdk/robot/led.py
@@ -59,7 +59,7 @@ class LEDController:
         if 'interval' in payload.keys():
             color_msg.frequency = float(payload['interval'])
         else:
-            color_msg.frequency = 1.0
+            color_msg.frequency = 0.0
         color_msg.color.a = float(payload['brightness']) / 100
         color_msg.color.r = self.normalize(r)
         color_msg.color.g = self.normalize(g)
